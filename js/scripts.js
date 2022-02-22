1860e151e3581edfa1d4ce91385248b94bd950c0
sfk <kashfi.sf@gmail.com>
2022-02-22 15:22:50 +0330

    release of landing page of Yalda!

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+$("#owl2").owlCarousel({
+	loop:true,
+	rtl: true,
+    margin:10,
+	nav:true,
+	dots: false,
+    responsive:{
+        0:{
+            items:2
+        },
+        400:{
+            items:3
+        },
+        768:{
+            items:5
+        },
+        1200:{
+            items:6
+        }
+        
+	}
+	
+})
+
